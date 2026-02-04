2cb630aa69d66772db99b3c0691c2cbf280476e7
Per-Christian Nielsen <per-christian.nielsen@nav.no>
2026-02-04 12:50:04 +0100

    Reusing kafkaProduser correctly

--- src/main/kotlin/no/nav/kafka/KafkaSender.kt
+++ src/main/kotlin/no/nav/kafka/KafkaSender.kt
@@ -4,19 +4,25 @@ import no.nav.config.KafkaConfig
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
 
-class KafkaSender: KafkaSenderInterface {
+class KafkaSender : KafkaSenderInterface {
     private val kafkaConfig = KafkaConfig()
     private val kafkaProducer = KafkaProducer<String, String>(kafkaConfig.producerProperties())
 
+    init {
+        Runtime.getRuntime().addShutdownHook(
+            Thread {
+                kafkaProducer.close()
+            },
+        )
+    }
+
     override fun sendToKafka(dataType: String, jsonBlob: String) {
-        kafkaProducer.use {
-            it.send(
-                ProducerRecord(
-                    kafkaConfig.tptTopic,
-                    dataType,
-                    jsonBlob,
-                ),
-            )
-        }
+        kafkaProducer.send(
+            ProducerRecord(
+                kafkaConfig.tptTopic,
+                dataType,
+                jsonBlob,
+            ),
+        )
     }
 }
