96bc9385894a3f7bd6c53a34a1da9cbe79d72ae8
J-K. Solbakken <jan.kare.solbakken@nav.no>
2026-07-08 14:33:50 +0200

    Forgot to add some files

---
+++ src/main/kotlin/no/nav/datastore/Datastore.kt
@@ -0,0 +1,20 @@
+package no.nav.datastore
+
+import org.neo4j.driver.Driver
+
+interface Datastore {
+    fun containersAbleToRunAsRoot(originRepo: String): List<String>
+}
+
+class Neo4jDatastore(val driver: Driver): Datastore {
+    override fun containersAbleToRunAsRoot(originRepo: String): List<String> {
+        val result = driver.executableQuery("""MATCH (:GitHubRepository {name: "whodis"})<-[*..3]-(c:KubernetesContainer{run_as_non_root: false}) RETURN DISTINCT c.name""").execute()
+        return result.records().map { it["name"].asString() }
+    }
+}
+
+class DummyDatastore: Datastore {
+    override fun containersAbleToRunAsRoot(originRepo: String): List<String> {
+        return emptyList()
+    }
+}
