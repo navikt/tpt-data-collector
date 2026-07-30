2b53ac8ceb252f54a563b3458cd34685b6f5f694
J-K. Solbakken <jan.kare.solbakken@nav.no>
2026-07-30 14:38:25 +0200

    Debugging

--- src/main/kotlin/no/nav/Main.kt
+++ src/main/kotlin/no/nav/Main.kt
@@ -85,6 +85,7 @@ fun Application.businessModule(gitHub: GitHub, datastore: Datastore, kafka: Kafk
     val teamSlugPattern = Regex("^[a-z0-9][a-z0-9-]*$")
 
     install(Authentication) {
+        println("Obtaining JWKS from ${config.openIdJwksUri}")
         val jwkProvider = JwkProviderBuilder(config.openIdJwksUri)
             .cached(10, 24, TimeUnit.HOURS)
             .rateLimited(20, 1, TimeUnit.MINUTES)
