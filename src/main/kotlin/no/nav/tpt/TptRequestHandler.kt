a9c3a3ebd0c00b4c5a11020591da76e9a8a9c6d6
J-K. Solbakken <jan.kare.solbakken@nav.no>
2026-08-31 11:48:39 +0200

    Workaround for quirk in the GitHub API

--- src/main/kotlin/no/nav/tpt/TptRequestHandler.kt
+++ src/main/kotlin/no/nav/tpt/TptRequestHandler.kt
@@ -1,5 +1,6 @@
 package no.nav.tpt
 
+import io.ktor.util.logging.KtorSimpleLogger
 import kotlinx.serialization.json.Json
 import no.nav.checks.CheckResultsForRepo
 import no.nav.checks.Checks
@@ -8,15 +9,22 @@ import no.nav.kafka.KafkaSenderInterface
 import no.nav.metrics.TPTMetrics
 
 class TptRequestHandler(private val gitHub: GitHub, private val checks: Checks, private val kafka: KafkaSenderInterface) {
+    val logger = KtorSimpleLogger(this::class.java.name)
 
-    suspend fun runAllChecksFor(teamSlug: String) =
+    suspend fun runAllChecksFor(teamSlug: String) {
         gitHub.allReposForTeam(teamSlug).map { repo ->
-            val allFilesInRepo = gitHub.allFilePathsIn(repo)
+            // GitHub returns 409 instead of an empty list if there are no files in a repo
+            val allFilesInRepo = try {
+                gitHub.allFilePathsIn(repo)
+            } catch (ex: Exception) {
+                logger.warn("Error while listing files in GitHub repo $repo", ex)
+                emptyList()
+            }
             val checkResults = checks.runAll(repo, allFilesInRepo.toSet())
             CheckResultsForRepo(repo, listOf(teamSlug), checkResults)
-        }.forEach {
-            kafka.sendToKafka("CheckResult", Json.encodeToString(it))
+        }.forEach { checkResultForRepo ->
+            kafka.sendToKafka("CheckResult", Json.encodeToString(checkResultForRepo))
             TPTMetrics.msgsSentToTpt(1)
         }
-
+    }
 }
